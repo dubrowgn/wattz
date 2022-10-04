1848cbdfd1ace59858880fdcf0a67c055c405858
Dustin Brown <me@dubrowgn.com>
2022-10-04 10:58:21 -0600

    Add live preview for invertCurrent setting

--- app/src/main/java/dubrowgn/wattz/Battery.kt
+++ app/src/main/java/dubrowgn/wattz/Battery.kt
@@ -26,16 +26,27 @@ class Battery(ctx: Context) {
         }
     }
 
-    private fun prop(id: Int): Double {
-        return mgr.getLongProperty(id).toDouble()
+    private fun prop(id: Int): Long? {
+        val v = mgr.getLongProperty(id)
+        if (v == Long.MIN_VALUE)
+            return null
+
+        return v
     }
 
-    private fun prop(id: String): Double? {
+    private fun prop(id: String): Long? {
         if (intent?.hasExtra(id) != true)
             return null
 
-        return intent.getIntExtra(id, Int.MIN_VALUE).toDouble()
+        val v = intent.getIntExtra(id, Int.MIN_VALUE)
+        if (v == Int.MIN_VALUE)
+            return null
+
+        return v.toLong()
     }
+
+    private fun propd(id: Int): Double? = prop(id)?.toDouble()
+    private fun propd(id: String): Double? = prop(id)?.toDouble()
 
     private fun Double?.times(v: Double?) : Double? {
         if (v == null)
@@ -52,32 +63,43 @@ class Battery(ctx: Context) {
         return v?.div(1_000.0)
     }
 
-    private fun toMillis(v: Double) : Double {
-        return v.times(1_000.0)
+    private fun toMillis(v: Double?) : Double? {
+        return v?.times(1_000.0)
     }
 
-    val milliamps : Double get() = toMillis(amps)
-    val amps : Double get() {
+    val currentRaw : Long? get() = prop(BatteryManager.BATTERY_PROPERTY_CURRENT_NOW)
+
+    val milliamps : Double? get() = toMillis(amps)
+    val amps : Double? get() {
         val sign = if (invertCurrent) 1.0 else -1.0
-        val amps = sign * prop(BatteryManager.BATTERY_PROPERTY_CURRENT_NOW)
+        val amps = sign.times(currentRaw?.toDouble())
+            ?: return null
+
         tuneAmpScalar(amps)
 
         return amps * ampScalar
     }
 
-    val millivolts : Double? get() = prop(BatteryManager.EXTRA_VOLTAGE)
+    val millivolts : Double? get() = propd(BatteryManager.EXTRA_VOLTAGE)
     val volts : Double? get() = fromMillis(millivolts)
 
     val milliwatts : Double? get() = milliamps.times(volts)
     val watts : Double? get() = amps.times(volts)
 
-    val energyAmpHours : Double? get() = fromMicros(prop(BatteryManager.BATTERY_PROPERTY_CHARGE_COUNTER))
+    val energyAmpHours : Double? get() = fromMicros(propd(BatteryManager.BATTERY_PROPERTY_CHARGE_COUNTER))
     val energyWattHours : Double? get() = volts?.times(energyAmpHours)
 
-    val celsius : Double? get() = prop(BatteryManager.EXTRA_TEMPERATURE)?.div(10.0)
+    val celsius : Double? get() = propd(BatteryManager.EXTRA_TEMPERATURE)?.div(10.0)
 
     // Some devices always report false for isCharging, so fall back to battery current detection
-    val charging : Boolean get() = mgr.isCharging || milliamps < 1.0
+    val charging : Boolean get() {
+        if (mgr.isCharging)
+            return true
+
+        val ma = milliamps
+        return ma != null && ma < 1.0
+    }
+
     val secondsUntilCharged: Double? get() {
         // Some devices incorrectly report "0 seconds to full" when not charging,
         // so ensure we are actually charging first.
--- app/src/main/java/dubrowgn/wattz/SettingsActivity.kt
+++ app/src/main/java/dubrowgn/wattz/SettingsActivity.kt
@@ -7,14 +7,19 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Switch
+import android.widget.TextView
 
 
 const val settingsName = "settings"
 const val settingsChannel = "dubrowgn.wattz.settings"
 
 class SettingsActivity : Activity() {
+    private lateinit var battery: Battery
+    private val task = PeriodicTask({ update() }, intervalMs)
+
     @SuppressLint("UseSwitchCompatOrMaterialCode")
     private lateinit var invertCurrent: Switch
+    private lateinit var invertCurrentPreview: TextView
 
     private fun debug(msg: String) {
         Log.d(this::class.java.name, msg)
@@ -26,7 +31,9 @@ class SettingsActivity : Activity() {
     }
 
     @SuppressLint("ApplySharedPref")
-    private fun storePrefs() {
+    private fun onChange() {
+        update()
+
         getSharedPreferences(settingsName, MODE_PRIVATE)
             .edit()
             .putBoolean("invertCurrent", invertCurrent.isChecked)
@@ -35,17 +42,42 @@ class SettingsActivity : Activity() {
         sendBroadcast(Intent().setAction(settingsChannel))
     }
 
+    private fun update() {
+        val current = battery.currentRaw
+        val sign = if(invertCurrent.isChecked) -1L else 1L
+        invertCurrentPreview.text = "${current?.times(sign)}"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         debug("onCreate()")
 
         super.onCreate(savedInstanceState)
 
+        battery = Battery(this)
+
         setContentView(R.layout.activity_settings)
 
         invertCurrent = findViewById(R.id.invertCurrent)
-        invertCurrent.setOnCheckedChangeListener { _, _ -> storePrefs() }
+        invertCurrent.setOnCheckedChangeListener { _, _ -> onChange() }
+        invertCurrentPreview = findViewById(R.id.invertCurrentPreview)
 
         loadPrefs()
+    }
+
+    override fun onPause() {
+        debug("onPause()")
+
+        task.stop()
+
+        super.onPause()
+    }
+
+    override fun onResume() {
+        debug("onResume()")
+
+        super.onResume()
+
+        task.start()
     }
 
     override fun onDestroy() {
