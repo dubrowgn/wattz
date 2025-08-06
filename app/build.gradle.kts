ac9a8caf629405d3deb8c6d39351fd941adaf35d
Dustin Brown <me@dubrowgn.com>
2025-08-06 13:00:02 -0600

    Release v1.20

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         // work around unused library resources
         resourceConfigurations.addAll(listOf("anydpi", "en", "es"))
         targetSdk = 34
-        versionCode = 19
-        versionName = "1.19"
+        versionCode = 20
+        versionName = "1.20"
     }
 
     buildTypes {
